bd2eb9e411bc81d76c3b77f3a4de8e9422e187f9
sahanasriram19 <sahana19.25@ichat.sp.edu.sg>
2026-07-16 17:02:18 +0800

    updated env and db

--- backend/src/services/db.js
+++ backend/src/services/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     ssl: {
         minVersion: 'TLSv1.2'
     },
