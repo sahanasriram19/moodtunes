9cea5f69d2f838c49945f1b59522003be8feac75
sahanasriram19 <sahana19.25@ichat.sp.edu.sg>
2026-07-16 16:49:53 +0800

    updated env and db

--- backend/src/services/db.js
+++ backend/src/services/db.js
@@ -2,10 +2,14 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host:            process.env.DB_HOST,
-    user:            process.env.DB_USER,
-    password:        process.env.DB_PASSWORD,
-    database:        process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        minVersion: 'TLSv1.2'
+    },
     multipleStatements: true,
     dateStrings: true,
     waitForConnections: true,
@@ -13,7 +17,6 @@ const pool = mysql.createPool({
     enableKeepAlive: true,
     keepAliveInitialDelay: 0
 });
-
 pool.on('error', function(err) {
     console.error('DB pool error:', err);
 });
